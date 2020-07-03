98e4d74f0601d6f44387d7d7e059d0c8ac367d74
Alex Ling <22086776+alexling540@users.noreply.github.com>
2020-07-03 18:03:34 -0500

    AppBar sticks to top without offset

--- DotNetCoreReactImplementation/ClientApp/src/components/AppBar.js
+++ DotNetCoreReactImplementation/ClientApp/src/components/AppBar.js
@@ -12,34 +12,33 @@ import logo from "../assets/coraLogo.svg";
 
 export const AppBar = () => {
   return (
-    <header className="AppBar">
-      <Navbar
-        variant="light"
-        bg="light"
-        fixed="top"
-        className="justify-content-between"
-      >
-        <Navbar.Brand as={Link} to="/" className="ml-3 ">
-          <Image src={logo} className={styles.logo} alt="CORAbot logo" />
-        </Navbar.Brand>
-        <Nav className="">
-          <Nav.Link as={Link} to="/about">
-            About
-          </Nav.Link>
-          <Nav.Link as={Link} to="/partners">
-            Partner Program
-          </Nav.Link>
-          <Nav.Link as={Link} to="/map">
-            Resources
-          </Nav.Link>
-          <Nav.Link as={Link} to="/needs">
-            Satisfying Needs
-          </Nav.Link>
-          <Button as={Link} to="/contact" variant="primary">
-            Contact
-          </Button>
-        </Nav>
-      </Navbar>
-    </header>
+    <Navbar
+      variant="light"
+      bg="light"
+      sticky="top"
+      className="AppBar justify-content-between"
+      as="header"
+    >
+      <Navbar.Brand as={Link} to="/" className="ml-3 ">
+        <Image src={logo} className={styles.logo} alt="CORAbot logo" />
+      </Navbar.Brand>
+      <Nav className="">
+        <Nav.Link as={Link} to="/about">
+          About
+        </Nav.Link>
+        <Nav.Link as={Link} to="/partners">
+          Partner Program
+        </Nav.Link>
+        <Nav.Link as={Link} to="/map">
+          Resources
+        </Nav.Link>
+        <Nav.Link as={Link} to="/needs">
+          Satisfying Needs
+        </Nav.Link>
+        <Button as={Link} to="/contact" variant="primary">
+          Contact
+        </Button>
+      </Nav>
+    </Navbar>
   );
 };
