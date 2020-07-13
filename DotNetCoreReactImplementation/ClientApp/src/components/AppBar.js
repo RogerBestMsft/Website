a904aa4aae037080da42cdd352c1c5c583e7a561
Alex Ling <22086776+alexling540@users.noreply.github.com>
2020-07-12 21:58:31 -0500

    Updated AppBar, Footer styling

--- DotNetCoreReactImplementation/ClientApp/src/components/AppBar.js
+++ DotNetCoreReactImplementation/ClientApp/src/components/AppBar.js
@@ -1,14 +1,18 @@
 import React from "react";
 
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 import Nav from "react-bootstrap/Nav";
 import Image from "react-bootstrap/Image";
 import Navbar from "react-bootstrap/Navbar";
 import Button from "react-bootstrap/Button";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 import styles from "./AppBar.module.css";
 import logo from "../assets/coraLogo.svg";
+// newer logo
+// for some reason the font is times new roman/serif not the original font
+import logo2 from "../assets/Logo.svg"; 
 
 export const AppBar = () => {
   return (
@@ -16,26 +20,28 @@ export const AppBar = () => {
       variant="light"
       bg="light"
       sticky="top"
-      className="AppBar justify-content-between"
+      className={`${styles.AppBar} justify-content-between`}
       as="header"
     >
-      <Navbar.Brand as={Link} to="/" className="ml-3 ">
+      <Navbar.Brand as={NavLink} to="/home" className="ml-3 ">
         <Image src={logo} className={styles.logo} alt="CORAbot logo" />
       </Navbar.Brand>
-      <Nav className="">
-        <Nav.Link as={Link} to="/about">
-          About
+      <Nav className={styles.navb}>
+        <NavDropdown title="About" className={styles.navLink}>
+          <NavDropdown.Item as={NavLink} to="/about" activeClassName="active">
+            Our Team
+          </NavDropdown.Item>
+          <NavDropdown.Item as={NavLink} to="/learn" activeClassName="active">
+            Technology
+          </NavDropdown.Item>
+        </NavDropdown>
+        <Nav.Link as={NavLink} className={styles.navLink} to="/map" activeClassName="active">
+          Map
         </Nav.Link>
-        <Nav.Link as={Link} to="/partners">
-          Partner Program
+        <Nav.Link as={NavLink} className={styles.navLink} to="/needs" activeClassName="active">
+          Case Studies
         </Nav.Link>
-        <Nav.Link as={Link} to="/map">
-          Resources
-        </Nav.Link>
-        <Nav.Link as={Link} to="/needs">
-          Satisfying Needs
-        </Nav.Link>
-        <Button as={Link} to="/contact" variant="primary">
+        <Button as={NavLink} className={styles.navLink} to="/contact" variant="primary">
           Contact
         </Button>
       </Nav>
